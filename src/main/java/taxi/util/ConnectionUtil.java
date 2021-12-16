7de91c998b0ac93c28bb2696f4d4d30d95ab5b18
Yaroslav Kavun <yka40znp@gmail.com>
2021-12-16 10:42:06 +0200

    the same problem from tomorrow with 2 files

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -16,7 +16,6 @@ public class ConnectionUtil {
             Class.forName(JDBC_DRIVER);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can't find SQL Driver", e);
-
         }
     }
 
